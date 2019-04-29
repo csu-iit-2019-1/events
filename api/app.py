376c824523fb8bc2a31b320933b5eed866adf5b1
starminalush <anastasiya.i@shop-loyalty.ru>
2019-04-30 00:09:22 +0500

    add exceptions

--- api/app.py
+++ api/app.py
@@ -1,4 +1,6 @@
 import json
+
+import requests
 from flask import Flask, request
 from controllers.buyout_controller import buyout_booking
 from controllers.getting_events_controller import get_events, get_event_by_id
@@ -23,16 +25,19 @@ def getEvent(event_id):
 
 @app.route('/events/booking/<event_id>', methods=['POST'])
 def bookingEvent(event_id):
-    user_id = request.form['userId']
-    count_adults = request.form['countOfPersonsAdults']
-    counts_children = request.form['countOfPersonsChildren']
-    return create_booking(event_id, user_id, int(count_adults) + int(counts_children))
+    try:
+        user_id = request.form['userId']
+        count_adults = request.form['countOfPersonsAdults']
+        counts_children = request.form['countOfPersonsChildren']
+        return str(create_booking(event_id, user_id, int(count_adults) + int(counts_children)))
+    except requests.RequestException:
+        return "Invalid Data - no such free seats"
+
 
 
 @app.route('/events/byuing/<booking_id>', methods=['GET'])
 def byuingEvent(booking_id):
     return buyout_booking(booking_id)
-
 
 if __name__ == '__main__':
     app.run(host=ADDRESS, port=PORT)
--- api/test_request.py
+++ api/test_request.py
@@ -15,4 +15,6 @@ print(json.loads(r))
 r = requests.get('http://127.0.0.1:5000/events/byuing/1').content
 print(r)
 r = requests.post('http://127.0.0.1:5000/events/booking/1',
-                  data={'userId': 456, "countOfPersonsAdults": 4, "countOfPersonsChildren": 1})
+                  data={'userId': 456, "countOfPersonsAdults": 108, "countOfPersonsChildren": 1})
+
+print(r)
