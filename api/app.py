2867fee73453a2eae50abb6466ec41d73140c0a5
starminalush <anastasiya.i@shop-loyalty.ru>
2019-04-29 13:37:44 +0500

    fix api methods

--- api/app.py
+++ api/app.py
@@ -1,4 +1,10 @@
+import json
+from http.client import HTTPResponse
+
 from flask import Flask, request
+
+from controllers.buyout_controller import buyout_booking
+from controllers.getting_events_controller import get_events
 from database.select_data import selectEvents, createBooking
 from config import ADDRESS, PORT
 
@@ -7,11 +13,10 @@ app = Flask(__name__)
 
 @app.route('/events/getEvents', methods=['POST'])
 def getEvents():
-    city = request.args.get('city')
+    city = request.args.get('cityId')
     startDate = request.args.get('startDate')
     endDate = request.args.get('endDate')
-
-    return selectEvents(city, startDate, endDate)
+    return get_events(city, startDate, endDate)
 
 
 @app.route('events/booking/<event_id>', methods=['GET'])
@@ -21,8 +26,7 @@ def bookingEvent(event_id, count_adults, count_children):
 
 @app.route('events/byuing/<booking_id>', methods=['GET'])
 def byuingEvent(booking_id):
-    # посылаем запрос на сервер авторизации
-    pass
+    return buyout_booking(booking_id)
 
 
 if __name__ == '__main__':
