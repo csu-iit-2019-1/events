c9f1d96ad9b866c6325cd9dfe0552d8ed5d1d20d
starminalush <anastasiya.i@shop-loyalty.ru>
2019-04-29 14:07:36 +0500

    add method for getting one event

--- api/app.py
+++ api/app.py
@@ -4,8 +4,8 @@ from http.client import HTTPResponse
 from flask import Flask, request
 
 from controllers.buyout_controller import buyout_booking
-from controllers.getting_events_controller import get_events
-from database.select_data import selectEvents, createBooking
+from controllers.getting_events_controller import get_events, get_event_by_id
+from controllers.booking_controller import createBooking
 from config import ADDRESS, PORT
 
 app = Flask(__name__)
@@ -16,7 +16,12 @@ def getEvents():
     city = request.args.get('cityId')
     startDate = request.args.get('startDate')
     endDate = request.args.get('endDate')
-    return get_events(city, startDate, endDate)
+    return json.dumps(get_events(city, startDate, endDate))
+
+
+@app.route('/events/getEvent/<event_id>', methods=['GET'])
+def getEvent(event_id):
+    return json.dumps(get_event_by_id(event_id))
 
 
 @app.route('events/booking/<event_id>', methods=['GET'])
