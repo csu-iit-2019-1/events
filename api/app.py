f767083eac703085d45f7e5edc20d7186bd9b01c
starminalush <anastasiya.i@shop-loyalty.ru>
2019-04-30 21:23:20 +0500

    add exceptions for api methods

--- api/app.py
+++ api/app.py
@@ -12,16 +12,25 @@ app = Flask(__name__)
 
 @app.route('/events/getEvents', methods=['POST'])
 def get_events():
-    request_params = request.get_json()
-    city = request_params['cityId']
-    startDate = request_params['startDate']
-    endDate = request_params['endDate']
-    return json.dumps(get_events(city, startDate, endDate))
+    try:
+        request_params = request.get_json()
+        city = request_params['cityId']
+        start_date = request_params['startate']
+        end_date = request_params['endDate']
+    except requests.exceptions.HTTPError:
+        return "Invalid request data"
+    try:
+        return json.dumps(get_events(city, start_date, end_date))
+    except ValueError:
+        return "Cannot get events for period"
 
 
 @app.route('/events/<event_id>', methods=['GET'])
 def get_event(event_id):
-    return json.dumps(get_event_by_id(event_id))
+    try:
+        return json.dumps(get_event_by_id(event_id))
+    except ValueError:
+        return "Event not found"
 
 
 @app.route('/events/booking/<event_id>', methods=['POST'])
@@ -31,15 +40,25 @@ def book_event(event_id):
         user_id = params_data['userId']
         count_adults = params_data['countOfPersonsAdults']
         counts_children = params_data['countOfPersonsChildren']
+    except requests.exceptions.HTTPError:
+        return "Invalid request data"
+    try:
         result = str(create_booking(event_id, user_id, int(count_adults) + int(counts_children)))
         return result
-    except requests.RequestException:
-        return "Invalid Data - no such free seats"
+    except ValueError:
+        return "Not enough free space"
 
 
-@app.route('/events/byuing/<booking_id>', methods=['GET'])
-def buy_event(booking_id):
-    return buyout_booking(booking_id)
+@app.route('/events/byuing', methods=['POST'])
+def buy_event():
+    try:
+        booking_id = request.get_json()[0]
+    except requests.exceptions.HTTPError:
+        return "Invalid request data"
+    try:
+        return buyout_booking(booking_id)
+    except ValueError:
+        return "Invalid booking id"
 
 
 if __name__ == '__main__':
